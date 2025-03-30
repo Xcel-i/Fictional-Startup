20997a5cbbde0a91206e1afd8df126532f9d1bad
ezedike evan <egwomevan323@gmail.com>
2025-03-30 01:31:14 +0100

    FIX:meet the founders section has been reworked

--- script.js
+++ script.js
@@ -26,3 +26,34 @@ $(".nav-icon").addEventListener("click",()=>{
 });
 const year = new Date().getFullYear();
 $("#date").innerText=year;
+
+
+//founders
+// Array to store image data
+const imageData = [
+  {
+    path: "./images/evan.jpg",
+    name : "Ezedike-Egwom Evan",
+    title: "Frontend Developer | Co-Founder",
+  },
+]
+
+// Function to display images dynamically
+function displayImages() {
+  const container = document.getElementById("cardContainer")
+  imageData.forEach((item)=>{
+      const card = document.createElement("div")
+      card.classList.add("card")
+
+      card.innerHTML = `
+        <img src="${item.path}" alt="${item.title}" class="card-img">
+        <h3>${item.name}</h3>
+        <h5>${item.title}</h5>
+      `
+
+      container.appendChild(card) 
+  });
+}
+
+// Call the function to display the images after the DOM has loaded
+document.addEventListener("DOMContentLoaded", displayImages);
